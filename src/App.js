45bc73835da9b801f143dc0df519d044616ab96b
catfishona <1972034@maranatha.ac.id>
2022-03-27 14:04:33 +0800

    Used map to display tracks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import './App.css';
-import Data from './data/Sample';
-import { SongCard } from './components/SongCard/index';
+import 'App.css';
+import datas from 'data/Sample';
+import { SongCard } from 'components/SongCard/index';
 
 function App() {
-
-  console.log(Data);
+  console.log(datas);
+  const trackList = datas.map((data, id) => 
+    <SongCard key={id} image={data.album.images[0].url} title={data.name} artist={data.album.artists[0].name} album={data.album.name} />
+  );
 
   return (
     <div>
     <h1>Make a Playlist!</h1>
-
-      {/* From git source */}
-      <SongCard image={Data.album.images[0].url} title={Data.name} artist={Data.album.artists[0].name} album={Data.album.name} />
-
+      {trackList};
       </div>
   );
 }

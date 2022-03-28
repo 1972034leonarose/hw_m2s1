0c0944ee18b244c951a3fbf5b79308899b16d90d
catfishona <1972034@maranatha.ac.id>
2022-03-28 14:38:15 +0800

    change to table structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import 'App.css';
-import datas from 'data/Sample';
-import { SongCard } from 'components/SongCard/index';
+import "App.css";
+import datas from "data/Sample";
+import { SongCard } from "components/SongCard/index";
 
 function App() {
   console.log(datas);
-  const trackList = datas.map((data, id) => 
-    <SongCard key={id} image={data.album.images[0].url} title={data.name} artist={data.album.artists[0].name} album={data.album.name} />
-  );
+  const trackList = datas.map((data, id) => (
+    <SongCard
+      key={id}
+      image={data.album.images[0].url}
+      title={data.name}
+      artist={data.album.artists[0].name}
+      album={data.album.name}
+    />
+  ));
 
   return (
-    <div>
-    <h1>Make a Playlist!</h1>
-      {trackList};
-      </div>
+    <div className="container">
+      <h1>Your Playlist</h1>
+      <button className="btn-select">Select</button>
+      <table className="song-list">
+        <tbody>
+          <tr>
+            <th className="album-img">Title</th>
+            <th className="song-item title"></th>
+            <th className="song-item artist">Artist</th>
+            <th className="song-item album">Album</th>
+          </tr>
+        </tbody>
+        {trackList}
+      </table>
+    </div>
   );
 }
 

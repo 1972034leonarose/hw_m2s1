8018b8a669c51e125a54eb8aa10cc4ef262647bb
catfishona <1972034@maranatha.ac.id>
2022-03-30 09:13:17 +0800

    add useStates and spotify api call

--- src/App.js
+++ src/App.js
@@ -1,34 +1,90 @@
+import React, { useState, useEffect } from "react";
 import "App.css";
-import datas from "data/Sample";
-import { SongCard } from "components/SongCard/index";
+import axios from "axios";
+import Track from "pages/Track";
 
 function App() {
-  console.log(datas);
-  const trackList = datas.map((data, id) => (
-    <SongCard
-      key={id}
-      image={data.album.images[0].url}
-      title={data.name}
-      artist={data.album.artists[0].name}
-      album={data.album.name}
-    />
-  ));
+  const CLIENT_ID = "bfa3638f86ad48c1972f2b90b2f45ae7";
+  const REDITECT_URI = "http://localhost:3000";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+
+  const [token, setToken] = useState("");
+  const [searchParam, setSearchParam] = useState("");
+  const [songs, setTracks] = useState([]);
+  const [submitted, setIsSubmitted] = useState(false);
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    setToken(token);
+  }, []);
+
+  // to clear token / local storage
+  const refresh = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+
+  const isClicked = () => setIsSubmitted(true);
+
+  // call spotify API
+  const callSpotifyApi = async (e) => {
+    // prevent form from submitting itself
+    e.preventDefault();
+    const { data } = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        q: searchParam,
+        type: "artist",
+      },
+    });
+    setTracks(data.artists.items);
+    console.log(data);
+  };
 
   return (
     <div className="container">
-      <h1>Your Playlist</h1>
-      <button className="btn-select">Select</button>
-      <table className="song-list">
-        <tbody>
-          <tr>
-            <th className="album-img">Title</th>
-            <th className="song-item title"></th>
-            <th className="song-item artist">Artist</th>
-            <th className="song-item album">Album</th>
-          </tr>
-        </tbody>
-        {trackList}
-      </table>
+      <h1>Home</h1>
+      {!token ? (
+        <a
+          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDITECT_URI}&response_type=${RESPONSE_TYPE}`}
+        >
+          To Spotify Auth
+        </a>
+      ) : (
+        <button onClick={refresh}>Refresh</button>
+      )}
+
+      {token ? (
+        <form onSubmit={callSpotifyApi}>
+          <input
+            type="text"
+            placeholder="Search for a song"
+            onChange={(e) => setSearchParam(e.target.value)}
+          ></input>
+          <button type={"submit"} onClick={isClicked}>Search</button>
+        </form>
+      ) : (
+        <h2> Not yet authorized </h2>
+      )}
+
+      {submitted ? <Track /> : null}
+
     </div>
   );
 }

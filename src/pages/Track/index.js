548442d69b33a57fe3c7db1f6ffbffcfce927798
catfishona <1972034@maranatha.ac.id>
2022-04-01 15:05:09 +0800

    comment unused component

--- src/pages/Track/index.js
+++ src/pages/Track/index.js
@@ -3,21 +3,22 @@ import datas from "data/Sample";
 import { SongCard } from "components/SongCard/index";
 
 function Track() {
-  console.log(datas);
-  const trackList = datas.map((data, id) => (
-    <SongCard
-      key={id}
-      image={data.album.images[0].url}
-      title={data.name}
-      artist={data.album.artists[0].name}
-      album={data.album.name}
-    />
-  ));
+  // console.log(datas);
+  // const trackList = datas.map((data, id) => (
+  //   <SongCard
+  //     key={id}
+  //     image={data.album.images[0].url}
+  //     title={data.name}
+  //     artist={data.album.artists[0].name}
+  //     album={data.album.name}
+  //     // TODO: recheck please
+  //     button={<button>Select</button>}
+  //   />
+  // ));
 
   return (
     <div className="container">
       <h1>Your Playlist</h1>
-      <button className="btn-select">Select</button>
       <table className="song-list">
         <tbody>
           <tr>
@@ -25,9 +26,10 @@ function Track() {
             <th className="song-item title"></th>
             <th className="song-item artist">Artist</th>
             <th className="song-item album">Album</th>
+            {/*space for button */}
+            <th></th>
           </tr>
         </tbody>
-        {trackList}
       </table>
     </div>
   );

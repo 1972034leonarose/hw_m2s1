77b1f074669acdf6ad4d8a5f74058198821242f8
catfishona <1972034@maranatha.ac.id>
2022-04-01 15:03:58 +0800

    display track instead, add map

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from "react";
 import "App.css";
 import axios from "axios";
 import Track from "pages/Track";
+import { SongCard } from "components/SongCard/index";
+
+/**
+ * to-do:
+ * fix select buttons to change per tr
+ * have selected songs appear when input field is cleared
+ */
 
 function App() {
   const CLIENT_ID = "bfa3638f86ad48c1972f2b90b2f45ae7";
@@ -11,8 +18,12 @@ function App() {
 
   const [token, setToken] = useState("");
   const [searchParam, setSearchParam] = useState("");
-  const [songs, setTracks] = useState([]);
+  const [tracks, setTracks] = useState([]);
   const [submitted, setIsSubmitted] = useState(false);
+
+  const [selected, setSelected] = useState("select");
+  const [selectedId, setSelectedId] = useState(""); // to store id of what's been selected
+  const [playlist, setPlaylist] = useState([]); // to store what's been selected
 
   useEffect(() => {
     const hash = window.location.hash;
@@ -28,12 +39,17 @@ function App() {
       window.location.hash = "";
       window.localStorage.setItem("token", token);
     }
-
     setToken(token);
   }, []);
 
+  const addToPlaylist = (song) => {
+    setPlaylist([...playlist, song]); // song = tracks.id
+    console.log("playlist:");
+    console.log(playlist);
+  };
+
   // to clear token / local storage
-  const refresh = () => {
+  const logout = () => {
     setToken("");
     window.localStorage.removeItem("token");
   };
@@ -41,51 +57,102 @@ function App() {
   const isClicked = () => setIsSubmitted(true);
 
   // call spotify API
-  const callSpotifyApi = async (e) => {
-    // prevent form from submitting itself
+  const getTracks = async (e) => {
     e.preventDefault();
-    const { data } = await axios.get("https://api.spotify.com/v1/search", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      params: {
-        q: searchParam,
-        type: "artist",
-      },
-    });
-    setTracks(data.artists.items);
-    console.log(data);
+    const datas = await axios
+      .get("https://api.spotify.com/v1/search", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        params: {
+          q: searchParam,
+          type: "track",
+        },
+      })
+      .then((response) => {
+        response ? setIsSubmitted(true) : setIsSubmitted(false);
+        console.log(submitted);
+        return response.data.tracks.items;
+      })
+      .catch((e) => console.log(e));
+
+    console.log(datas);
+    setTracks(datas);
   };
 
+  // TODO: utilize props ??
+  const isSelected = (id) => {
+    if (selected === "select") {
+      setSelected("deselect");
+    } else {
+      setSelected("select");
+    }
+  };
+
+  const mapTracks = tracks.map((song, id) => (
+    <SongCard
+      key={id}
+      uri={song.uri}
+      image={song.album.images[2].url}
+      title={song.name}
+      artist={song.artists[0].name}
+      album={song.album.name}
+      // TODO: recheck please
+      button={
+        <input
+          type="submit"
+          value={selected}
+          onClick={() => {
+            isSelected(tracks[id]);
+            console.log(tracks[id]);
+            addToPlaylist(tracks[id]);
+          }}  
+        />
+      }
+    />
+  ));
+
   return (
-    <div className="container">
-      <h1>Home</h1>
-      {!token ? (
-        <a
-          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDITECT_URI}&response_type=${RESPONSE_TYPE}`}
-        >
-          To Spotify Auth
-        </a>
-      ) : (
-        <button onClick={refresh}>Refresh</button>
-      )}
+    <>
+      <div className="container">
+        {!token ? (
+          <div className="btn-login">
+            <a
+              href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDITECT_URI}&response_type=${RESPONSE_TYPE}`}
+            >
+              Login
+            </a>
+          </div>
+        ) : (
+          <button className="btn-logout" onClick={logout}>
+            Logout
+          </button>
+        )}
 
-      {token ? (
-        <form onSubmit={callSpotifyApi}>
-          <input
-            type="text"
-            placeholder="Search for a song"
-            onChange={(e) => setSearchParam(e.target.value)}
-          ></input>
-          <button type={"submit"} onClick={isClicked}>Search</button>
-        </form>
-      ) : (
-        <h2> Not yet authorized </h2>
-      )}
-
-      {submitted ? <Track /> : null}
-
-    </div>
+        {token ? (
+          <form onSubmit={getTracks}>
+            <input
+              type="text"
+              placeholder="Search for a song"
+              value={searchParam}
+              onChange={(e) => {
+                setSearchParam(e.target.value);
+              }}
+            ></input>
+            <button type="submit" onClick={isClicked}>
+              Search
+            </button>
+          </form>
+        ) : (
+          <h2> Not yet authorized </h2>
+        )}
+        <>
+          <br />
+          <br />
+          {submitted ? tracks.map((song, id) => mapTracks) : null}
+        </>
+      </div>
+    </>
   );
 }
 

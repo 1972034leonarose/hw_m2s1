6fd411b3f7f76db882575a52acc22560a2236c07
catfishona <1972034@maranatha.ac.id>
2022-04-04 05:27:47 +0800

    edit with tailwind

--- src/components/molecules/PlaylistForm/index.js
+++ src/components/molecules/PlaylistForm/index.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React from "react";
 
 export const PlaylistForm = (props) => {
   return (
-    <div> 
-        <div className="title">
-            <input type="text" placeholder="title" onChange={props.input}></input>
-        </div>
-        <div className="description">
-            <textarea placeholder="Description" onChange={props.description}></textarea>
-        </div>
+    <div>
+      <form className="px-4 pt-6 pb-8 mb-4">
+        <input
+          className="text-sm shadow appearance-none border rounded w-48 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          placeholder="Title"
+          value={props.title}
+          onChange={props.input}
+          required
+        ></input>
+        <textarea
+          className="text-sm shadow appearance-none border rounded w-48 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-3"
+          placeholder="Description"
+          onChange={props.description}
+          required
+        ></textarea>
         <div>
-          <button type="text" onClick={props.createPlaylist}>Create Playlist</button>
+          <button className="text-sm font-bold bg-pink-600 hover:bg-pink-800 py-2 px-4 mt-3 rounded" type="text" onClick={props.createPlaylist}>
+            create
+          </button>
         </div>
+      </form>
+      
+      
     </div>
-  )
-}
-
+  );
+};

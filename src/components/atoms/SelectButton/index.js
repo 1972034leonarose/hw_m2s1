f3740dbb95c1724f22f32816b928a05e9542d435
catfishona <1972034@maranatha.ac.id>
2022-04-04 05:28:18 +0800

    edit with tailwind

--- src/components/atoms/SelectButton/index.js
+++ src/components/atoms/SelectButton/index.js
@@ -1,8 +1,12 @@
-
-const SelectButton = ({children, onClick}) => {
-    return (
-        <button onClick={onClick}>{children}</button>
-    )
-}
+const SelectButton = ({ children, onClick }) => {
+  return (
+    <button
+      className="w-24 text-center bg-pink-600 hover:bg-pink-600/75 text-white font-bold py-2 px-4 rounded"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default SelectButton;

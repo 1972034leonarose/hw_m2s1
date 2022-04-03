40702fe8932bfec28f62719799cad6460a4af78d
catfishona <1972034@maranatha.ac.id>
2022-04-04 05:25:04 +0800

    refactor landing page

---
+++ src/pages/LandingPage/index.js
@@ -0,0 +1,11 @@
+function LandingPage(props) {
+  return (
+    <div className="py-3">
+      <h1 className="text-5xl">welcome to</h1>
+      <h1 className="text-5xl font-bold py-3">playroll</h1>
+      <br/>
+    </div>
+  );
+}
+
+export default LandingPage;

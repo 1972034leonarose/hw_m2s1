d9b4a1ad45aaa9d3111237394e61b53e67b3ec8f
catfishona <1972034@maranatha.ac.id>
2022-04-04 01:07:53 +0800

    change component name to Home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Track from './pages/Track';
+import Home from './pages/Track';
 
 function App() {
   return (
     <div>
-    <Track/>
+    <Home />
     </div>
   )
 }

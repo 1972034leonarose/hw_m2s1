e00e8d2a0ad32c49c3f91b527ddda421cbb5fe79
catfishona <1972034@maranatha.ac.id>
2022-04-06 01:26:22 +0800

    add provider component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import './App.css';
-import Home from './pages/Home';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
     <div>
-    <Home />
+      <Provider store={store}>
+        <Home />
+      </Provider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

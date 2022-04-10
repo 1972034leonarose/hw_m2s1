4dfd6a5ee420829760c8066141cf53256fe27cca
catfishona <1972034@maranatha.ac.id>
2022-04-10 20:16:14 +0800

    docs: add proptypes

--- src/components/atoms/SelectButton/index.js
+++ src/components/atoms/SelectButton/index.js
@@ -1,12 +1,17 @@
-const SelectButton = ({ children, onClick }) => {
-  return (
-    <button
-      className="w-24 text-center bg-pink-600 hover:bg-pink-600/75 text-white font-bold py-2 px-4 rounded"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
-};
+import React from "react";
+import { func } from "prop-types";
 
+const SelectButton = ({ children, onClick }) => (
+  <button
+    className="w-24 text-center bg-pink-600 hover:bg-pink-600/75 text-white font-bold py-2 px-4 rounded"
+    onClick={onClick}
+  >
+    {children}
+  </button>
+);
+
+SelectButton.propTypes = {
+  children: func,
+  onClick: func,
+};
 export default SelectButton;

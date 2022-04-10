7312936aab47486003db979021b4120432748c4d
catfishona <1972034@maranatha.ac.id>
2022-04-11 02:01:01 +0800

    fix: select button states

--- src/components/molecules/SongCard/index.js
+++ src/components/molecules/SongCard/index.js
@@ -1,29 +1,25 @@
-import { useState } from "react";
+import React from "react";
 import "./styles.css";
 import SelectButton from "../../atoms/SelectButton";
 
-export const SongCard = (props) => {
-  const [selected, setSelected] = useState(false);
-
+export function SongCard(props) {
   const isSelected = () => {
-    setSelected(!selected);
     props.selectSong(); // function props
   };
 
   return (
-      <tr className="song-area">
-        <td className="album-img">
-          <img src={props.image} className="song-img" alt={props.title}></img>
+    <tr className="song-area">
+      <td className="album-img">
+          <img src={props.image} className="song-img" alt={props.title} />
         </td>
-        <td className="song-item title">{props.title}</td>
-        <td className="song-item artist">{props.artist}</td>
-        <td className="song-item album">{props.album}</td>
-        {/* <td className="btn-area">{props.button}</td> */}
-        <td>
+      <td className="song-item title">{props.title}</td>
+      <td className="song-item artist">{props.artist}</td>
+      <td className="song-item album">{props.album}</td>
+      <td>
           <SelectButton className="btn-area" type="button" onClick={isSelected}>
-            {selected ? "deselect" : "select"}
+            {props.isSelected ? "deselect" : "select"}
           </SelectButton>
         </td>
-      </tr>
+    </tr>
   );
-};
+}

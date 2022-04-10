ae551fd0d4900180b96ecd7adcbdeba8129a31d2
catfishona <1972034@maranatha.ac.id>
2022-04-11 02:03:55 +0800

    feat: add routing

--- src/route/route.js
+++ src/route/route.js
@@ -3,14 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
+import { useSelector } from "react-redux";
 
 function Routing() {
+  const { isAuthorized } = useSelector((state) => state.auth);
+
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        {isAuthorized && (<Route path="/home" element={<Home />} />)};
         <Route path="/" element={<LandingPage />} exact />
-        <Route path="*" element={<NotFound />}></Route>
+        {/* <Route path="*" element={<NotFound />}></Route> */}
       </Routes>
     </Router>
   );

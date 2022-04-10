60f13a2dcef0c095cafc9fb16d87a84c63191f53
catfishona <1972034@maranatha.ac.id>
2022-04-11 02:03:27 +0800

    fix: edit states

--- src/redux/slices.js
+++ src/redux/slices.js
@@ -1,26 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const authSlice = createSlice({
-    name: "authorize",
-    initialState: {
-        token: "",
-        isAuthorized: false,
-        profile: {}
+  name: "authorize",
+  initialState: {
+    token: "",
+    isAuthorized: false,
+    profile: {},
+  },
+  reducers: {
+    setToken: (state, action) => {
+      state.token = action.payload;
+      state.isAuthorized = true;
     },
-    reducers: {
-        setToken: (state, action) => {
-            state.token = action.payload;
-            state.isAuthorized = true;
-        },
-        setProfile: (state, action) => {
-            state.profile = action.payload;
-        },
-        removeToken: (state) => {
-            state.token = "";
-        }
-    }
+    setProfile: (state, action) => {
+      state.profile = action.payload;
+    },
+    removeToken: (state) => {
+      state.token = "";
+    },
+  },
 });
 
 export const { setToken, setProfile, removeToken } = authSlice.actions;
 export default authSlice.reducer;
- 
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices";
 
 export default configureStore({
-    reducer: {
-        auth: authReducer,
-    }
-})
+  reducer: {
+    auth: authReducer,
+  },
+});

150fe94bb997e48b24963bb3a9d51069666d9e55
catfishona <1972034@maranatha.ac.id>
2022-04-10 20:16:51 +0800

    add constants directory

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,6 +1,6 @@
 export const config = {
-    SPOTIFY_ENDPOINT_URL: 'https://api.spotify.com/v1/',
-    AUTH_ENDPOINT: 'https://accounts.spotify.com/authorize',
-    REDIRECT_URI: 'http://localhost:3000',
-    SCOPE: 'playlist-modify-private'
-}
+  SPOTIFY_ENDPOINT_URL: "https://api.spotify.com/v1/",
+  AUTH_ENDPOINT: "https://accounts.spotify.com/authorize",
+  REDIRECT_URI: "http://localhost:3000",
+  SCOPE: "playlist-modify-private",
+};

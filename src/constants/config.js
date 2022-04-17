1b8bcf667564baf1d6b14d83c6630fceb9e7f882
Leona Rose <79490349+catfishona@users.noreply.github.com>
2022-04-18 02:17:16 +0800

    remove unused config file

--- src/constants/config.js
+++
@@ -1,6 +0,0 @@
-export const config = {
-  SPOTIFY_ENDPOINT_URL: "https://api.spotify.com/v1/",
-  AUTH_ENDPOINT: "https://accounts.spotify.com/authorize",
-  REDIRECT_URI: "http://localhost:3000",
-  SCOPE: "playlist-modify-private",
-};

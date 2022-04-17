d7d74cfade0a42c26a012abcccf694b9ea25a8c5
catfishona <1972034@maranatha.ac.id>
2022-04-17 21:59:49 +0800

    refactor: clean up code

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,34 +1,18 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+
+// third-party
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { setToken } from "../../redux/slices";
 import HeadphonesIcon from "@mui/icons-material/Headphones";
 
+// components
+import { authorizeUser } from "../../lib/auth";
+import useToken from "../../lib/useToken";
+
 function LandingPage() {
-  const dispatch = useDispatch();
-  const { token, isAuthorized, profile } = useSelector((state) => state.auth);
   let navigate = useNavigate();
-
-  useEffect(() => {
-    const hash = window.location.hash;
-
-    if (!token && hash) {
-      const temp = hash
-        .substring(1)
-        .split("&")
-        .find((elem) => elem.startsWith("access_token"))
-        .split("=")[1];
-      dispatch(setToken(temp));
-    }
-  }, []);
-
-  const CLIENT_ID = "bfa3638f86ad48c1972f2b90b2f45ae7";
-  const REDITECT_URI = "http://localhost:3000";
-  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-  const RESPONSE_TYPE = "token";
-  const SCOPE = "playlist-modify-private";
-
-  const authUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDITECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+  let { token } = useToken();
+  let { isAuthorized, profile } = useSelector((state) => state.auth);
 
   return (
     <div className="my-56 mx-24">
@@ -42,12 +26,12 @@ function LandingPage() {
       </div>
 
       <div className="flex space-x-4 mt-4">
-        <a
+        <button
           className="text-lg w-42 text-center bg-pink-600 hover:bg-pink-600/75 text-white font-bold mt-3 py-2 px-3 rounded"
-          href={authUrl}
+          onClick={authorizeUser}
         >
-          Authorize{" "}
-        </a>
+          Authorize
+        </button>
 
         {/* TODO: not authorized; do cursor warning maybe*/}
         <button
@@ -58,9 +42,12 @@ function LandingPage() {
         >
           Create Playlist
         </button>
+
         {/* test bugs */}
+        {console.log(token)}
         {console.log(isAuthorized)}
-        {console.log(`Profile: ${profile.display_name}`)}
+        {console.log(profile)}
+
       </div>
     </div>
   );

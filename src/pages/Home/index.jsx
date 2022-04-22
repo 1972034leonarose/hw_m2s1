14894d640571ab6d457f9f47b70563c405b705b6
catfishona <1972034@maranatha.ac.id>
2022-04-22 23:56:02 +0800

    docs: add vercel dev

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import "../../App.css";
 import "./styles.css";
-
 // components & lib
 import useHandlers from "../../lib/useHandlers";
 import HomeSkeleton from "../../components/layouts/HomeSkeleton";

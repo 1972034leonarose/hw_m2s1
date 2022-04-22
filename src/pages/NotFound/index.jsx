af48345660664b7c6670555ef2e5e6ca496b00a9
catfishona <1972034@maranatha.ac.id>
2022-04-22 23:01:24 +0800

    style: edit not found page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <div><h1>Page Not Found</h1></div>
-  )
+    <div className="my-56 mx-24">
+      <section id="title">
+        <h1 className="text-5xl font-bold py-3">404</h1>
+        <h1 className="text-5xl">Page Not Found</h1>
+      </section>
+
+      <section id="subheading">
+        <h5 className="text-xl leading-normal mt-5 mb-4">
+          <Link to="/">Head home</Link>
+        </h5>
+      </section>
+    </div>
+  );
 }
 
 export default NotFound;

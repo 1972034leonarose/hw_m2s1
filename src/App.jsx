fc5249cdb249d5c5f6616c007130115d26d4f4f2
catfishona <1972034@maranatha.ac.id>
2022-04-22 23:03:07 +0800

    docs: change to jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+import { Provider } from "react-redux";
+import { Routing } from "./route/route";
+import store from "./redux/store";
+
+function App() {
+  return (
+    <div>
+      <Provider store={store}>
+        <Routing />
+      </Provider>
+    </div>
+  );
+}
+
+export default App;
